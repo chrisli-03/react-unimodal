bf9ab7118e7e26fa6208fec9e7bd7f2622b0bd56
Chris <11177048+HechengLi@users.noreply.github.com>
2022-07-09 14:45:19 -0400

    Update useUniModal.js

--- useUniModal.js
+++ useUniModal.js
@@ -1,22 +1,46 @@
-import { useReducer, useCallback } from 'react';
+import { useEffect, useReducer, useCallback, useMemo } from 'react';
 
 const defaultSimbol = Symbol('__default__');
-const listeners = [];
+let listeners = [];
+
+export const showModal = (id = defaultSimbol) => {
+  listeners.forEach(({ id: listenersId, displayCallback }) => {
+    if (listenersId === id) {
+      displayCallback(true);
+    }
+  });
+}
+
+export const hideModal = (id = defaultSimbol) => {
+  listeners.forEach(({ id: listenersId, displayCallback }) => {
+    if (listenersId === id) {
+      displayCallback(false);
+    }
+  });
+}
 
 export const updateModal = (config, id = defaultSimbol) => {
-  listeners.forEach(({ id: listenersId, callback }) => {
+  listeners.forEach(({ id: listenersId, updateCallback }) => {
     if (listenersId === id) {
-      callback(config);
+      updateCallback(config);
     }
   });
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'display_modal': {
+      return {
+        ...state,
+        open: action.payload.open
+      }
+    }
     case 'update_modal': {
       return {
         ...state,
-        body: action.payload.body
+        header: typeof action.payload.header === 'undefined' ? state.header : action.payload.header,
+        body: typeof action.payload.body === 'undefined' ? state.body : action.payload.body,
+        footer: typeof action.payload.footer === 'undefined' ? state.footer : action.payload.footer
       }
     }
     default:
@@ -25,36 +49,74 @@ const reducer = (state, action) => {
 }
 
 const UniModal = ({
-  body
+  open,
+  hideFn,
+  header,
+  body,
+  footer
 }) => {
+  if (!open) {
+    return null;
+  }
+
   return (
     <div className="uni-modal">
-      {body}
+      <button type="button" onClick={hideFn}>close</button>
+      {header && <div className="uni-modal__header">{header}</div>}
+      {body && <div className="uni-modal__body">{body}</div>}
+      {footer && <div className="uni-modal__footer">{footer}</div>}
     </div>
   )
 }
 
-const useUniModal = (id = defaultSimbol) => {
-  const [state, dispatch] = useReducer(reducer, { id, body: () => null });
-  listeners.push({
-    id,
-    callback: ({ body }) => {
-      dispatch({
-        type: 'update_modal',
-        payload: {
-          body
-        }
-      });
+export const useUniModal = (...params) => {
+  let id = defaultSimbol;
+  let config = {};
+  if (params.length === 1) {
+    if (typeof params[0] === 'string') {
+      id = params[0]
+    } else if (typeof params[0] === 'object') {
+      config = params[0]
     }
-  });
+  } else if (params.length > 1) {
+    [id, config] = params;
+  }
+  const [state, dispatch] = useReducer(reducer, { id, open: false, ...config });
+  useEffect(() => {
+    listeners.push({
+      id,
+      updateCallback: ({ header, body, footer }) => {
+        dispatch({
+          type: 'update_modal',
+          payload: {
+            header,
+            body,
+            footer
+          }
+        });
+      },
+      displayCallback: (open) => {
+        dispatch({
+          type: 'display_modal',
+          payload: {
+            open
+          }
+        })
+      }
+    });
+    return () => {
+      listeners = listeners.filter(({ id: listenerId }) => listenerId !== id );
+    }
+  }, [id]);
 
-  const body = typeof state.body === 'function' ? <state.body /> : state.body;
+  const open = useMemo(() => state.open, [state.open]);
+  const body = useMemo(() => typeof state.body === 'function' ? <state.body /> : state.body, [state.body]);
+  const header = useMemo(() => typeof state.header === 'function' ? <state.header /> : state.header, [state.header]);
+  const footer = useMemo(() => typeof state.footer === 'function' ? <state.footer /> : state.footer, [state.footer]);
 
   const memoizedCallback = useCallback(
-    () => <UniModal body={body} />,
-    [body],
+    () => <UniModal open={open} hideFn={() => hideModal(id)} header={header} body={body} footer={footer} />,
+    [id, open, header, body, footer],
   );
   return memoizedCallback;
 }
-
-export default useUniModal;
